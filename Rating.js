40c88ca6f6dc842e5e4b2bfb975e1c92d65d7627
vijayakumar M <mvijaykumarpbt@gmail.com>
2023-02-11 20:59:47 +0530

    Rating the restaurant

---
+++ Rating.js
@@ -0,0 +1,11 @@
+ratingData = [
+    {restaurant: 'KFC', rating:5},
+    {restaurant: 'Burger King', rating:4},
+    {restaurant: 'KFC', rating:3},
+    {restaurant: 'Domino', rating:2},
+    {restaurant: 'Subway', rating:3},
+    {restaurant: 'Domino', rating:1},
+    {restaurant: 'Subway', rating:4},
+    {restaurant: 'Pizza Hut', rating:5}
+    ] 
+    
